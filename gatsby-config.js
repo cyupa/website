9d04c8a03960a8c8a7d6ccd140778ba9e734a655
Bradley Ayers <brad@dovetailapp.com>
2018-03-29 16:25:28 +1100

    feat: add gatsby, tsconfig, husky, lint-staged

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+// Based on https://github.com/gatsbyjs/gatsby/blob/master/examples/using-typescript/gatsby-config.js
+module.exports = {
+  siteMetadata: {
+    siteName: `Dovetail`
+  },
+  plugins: [
+    // Add typescript stack into webpack
+    `gatsby-plugin-typescript`
+  ]
+};
