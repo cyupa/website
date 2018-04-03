ab2cb92bc148aac39f7e3dbb0058781e0db05abd
Bradley Ayers <brad@dovetailapp.com>
2018-04-04 08:07:08 +1000

    chore: add src/html.tsx

---
+++ src/html.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+
+// Load production style
+let styles: string;
+if (process.env.NODE_ENV === "production") {
+  try {
+    styles = require("!raw-loader!../public/styles.css");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+interface HtmlProps {
+  body: string;
+  postBodyComponents: React.ReactNode;
+  headComponents: React.ReactNode;
+}
+
+class Html extends React.Component<HtmlProps> {
+  public render() {
+    const css =
+      process.env.NODE_ENV === `production` ? (
+        <style id="gatsby-inlined-css" dangerouslySetInnerHTML={{ __html: styles }} />
+      ) : null;
+
+    return (
+      <html lang="en">
+        <head>
+          {this.props.headComponents}
+          <title>My website</title>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+          {css}
+        </head>
+        <body>
+          <div id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
+          {this.props.postBodyComponents}
+          HELLO WORLD
+        </body>
+      </html>
+    );
+  }
+}
+
+// Use `module.exports` to be compliante with `webpack-require` import method
+module.exports = Html;
