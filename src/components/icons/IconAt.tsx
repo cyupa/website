f911d88555262f3af197996329c5d8ec8ae5f613
Bradley Ayers <brad@dovetailapp.com>
2018-04-04 08:25:09 +1000

    chore: add example SVG icon

---
+++ src/components/icons/IconAt.tsx
@@ -0,0 +1,26 @@
+// tslint:disable
+//
+//  This file was generated automatically by @heydovetail/svg-to-react and
+//  should not be manually edited.
+//
+import { createSvg } from "@heydovetail/svg-to-react";
+import * as React from "react";
+
+export default createSvg((width = 24, height = 24, color) => {
+  return (
+    <svg width={width} height={height} viewBox="0 0 24 24">
+      <g
+        transform="translate(1 2)"
+        stroke={color}
+        strokeWidth={2}
+        fill="none"
+        fillRule="evenodd"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <circle cx={11} cy={10} r={4} />
+        <path d="M15 10v1a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94" />
+      </g>
+    </svg>
+  );
+});
