ee0f1a6852bd97f35375eeb8e2fb75259cd3f496
Bradley Ayers <brad@dovetailapp.com>
2018-04-12 17:22:43 +1000

    fix: jest and storybook support for binary image imports

--- .storybook/webpack.config.ts
+++ .storybook/webpack.config.ts
@@ -6,6 +6,10 @@ export const module = {
       test: /\.tsx?$/,
       exclude: /node_modules/,
       loader: "awesome-typescript-loader"
+    },
+    {
+      test: /\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$/,
+      loader: "file-loader"
     }
   ]
 };
