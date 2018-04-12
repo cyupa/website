c9f3e584fc0c9ae2f35bee981bbd14c9c36ea56c
Bradley Ayers <brad@dovetailapp.com>
2018-04-12 11:46:18 +1000

    fix: import React -> react

--- src/layouts/dark.tsx
+++ src/layouts/dark.tsx
@@ -1,4 +1,4 @@
-import * as React from "React";
+import * as React from "react";
 import { Footer } from "../components/site/Footer";
 import { Header } from "../components/site/Header";
 import { COLORS } from "../constants";
