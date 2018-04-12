d3717e3ba29540199c318caf18f38e2f1b89eb8d
Benjamin Humphrey <humphreybc@gmail.com>
2018-04-12 12:44:53 +1000

    fix: customer logos

--- src/sections/Customers/index.tsx
+++ src/sections/Customers/index.tsx
@@ -9,36 +9,39 @@ import LogoUniqa from "../../components/icons/customers/LogoUniqa";
 import LogoWestpac from "../../components/icons/customers/LogoWestpac";
 import { GridWrap } from "../../components/layout/GridWrap";
 import { Item } from "../../components/layout/Item";
+import { COLORS } from "../../constants";
 
 export class Customers extends React.PureComponent {
   public render() {
     return (
-      <GridWrap gap={64} justifyContent="center">
-        <Item>
-          <LogoAtlassian />
-        </Item>
-        <Item>
-          <LogoDeloitte />
-        </Item>
-        <Item>
-          <LogoNike />
-        </Item>
-        <Item>
-          <LogoUniqa />
-        </Item>
-        <Item>
-          <LogoCsiro />
-        </Item>
-        <Item>
-          <LogoEnigma />
-        </Item>
-        <Item>
-          <LogoFreshworks />
-        </Item>
-        <Item>
-          <LogoWestpac />
-        </Item>
-      </GridWrap>
+      <div style={{ color: COLORS.i60 }}>
+        <GridWrap gap={64} justifyContent="center">
+          <Item>
+            <LogoAtlassian />
+          </Item>
+          <Item>
+            <LogoDeloitte />
+          </Item>
+          <Item>
+            <LogoNike />
+          </Item>
+          <Item>
+            <LogoUniqa />
+          </Item>
+          <Item>
+            <LogoCsiro />
+          </Item>
+          <Item>
+            <LogoEnigma />
+          </Item>
+          <Item>
+            <LogoFreshworks />
+          </Item>
+          <Item>
+            <LogoWestpac />
+          </Item>
+        </GridWrap>
+      </div>
     );
   }
 }
