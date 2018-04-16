6e5d02b0c0f849dc0e798eaef58b49e8329cec3f
Benjamin Humphrey <humphreybc@gmail.com>
2018-04-16 12:22:40 +1000

    fix: Features item growth

--- src/components/site/Features/index.tsx
+++ src/components/site/Features/index.tsx
@@ -13,7 +13,7 @@ export class Features extends React.PureComponent<Props> {
   public render() {
     const { features, large = false } = this.props;
     return (
-      <GridWrap maxChildWidth={256} justifyContent="center" gap={large ? 64 : 32}>
+      <GridWrap growItems maxChildWidth={256} justifyContent="center" gap={large ? 64 : 32}>
         {features.map((f, i) => (
           <Item key={i}>
             {large ? <FeatureLarge text={f.text} title={f.title} /> : <Feature text={f.text} title={f.title} />}
