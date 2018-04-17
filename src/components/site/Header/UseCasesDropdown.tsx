9bd60a3823a77cb1ac7050aa9aaed62856d772a1
Benjamin Humphrey <humphreybc@gmail.com>
2018-04-17 12:43:37 +1000

    fix: Resolve two problems in Edge

--- src/components/site/Header/UseCasesDropdown.tsx
+++ src/components/site/Header/UseCasesDropdown.tsx
@@ -4,7 +4,8 @@ import PictogramQda from "@heydovetail/website/components/icons/pictograms/Picto
 import PictogramRepository from "@heydovetail/website/components/icons/pictograms/PictogramRepository";
 import PictogramResearch from "@heydovetail/website/components/icons/pictograms/PictogramResearch";
 import { Container } from "@heydovetail/website/components/layout/Container";
-import { Grid } from "@heydovetail/website/components/layout/Grid";
+import { GridWrap } from "@heydovetail/website/components/layout/GridWrap";
+import { Item } from "@heydovetail/website/components/layout/Item";
 import { locations } from "@heydovetail/website/routing/locations";
 import * as React from "react";
 import { UseCase } from "./UseCase";
@@ -14,24 +15,32 @@ export class UseCasesDropdown extends React.PureComponent {
     return (
       <Dropdown maxWidth={360}>
         <Container horizontalPadding={16} verticalPadding={8}>
-          <Grid gap={0}>
-            <UseCase
-              image={<PictogramFeedback />}
-              location={locations.customerFeedback()}
-              title="Customer feedback management"
-            />
-            <UseCase
-              image={<PictogramRepository />}
-              location={locations.researchRepository()}
-              title="User research repository"
-            />
-            <UseCase
-              image={<PictogramQda />}
-              location={locations.qualitativeDataAnalysis()}
-              title="Qualitative data analysis"
-            />
-            <UseCase image={<PictogramResearch />} location={locations.userResearch()} title="Collaborative user research" />
-          </Grid>
+          <GridWrap maxChildWidth={128} gap={0} growItems>
+            <Item>
+              <UseCase
+                image={<PictogramFeedback />}
+                location={locations.customerFeedback()}
+                title="Customer feedback management"
+              />
+            </Item>
+            <Item>
+              <UseCase
+                image={<PictogramRepository />}
+                location={locations.researchRepository()}
+                title="User research repository"
+              />
+            </Item>
+            <Item>
+              <UseCase
+                image={<PictogramQda />}
+                location={locations.qualitativeDataAnalysis()}
+                title="Qualitative data analysis"
+              />
+            </Item>
+            <Item>
+              <UseCase image={<PictogramResearch />} location={locations.userResearch()} title="Collaborative user research" />
+            </Item>
+          </GridWrap>
         </Container>
       </Dropdown>
     );
