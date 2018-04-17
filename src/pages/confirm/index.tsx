70ff8fec9acd82cf676404a52db966dfab39e68b
Benjamin Humphrey <humphreybc@gmail.com>
2018-04-17 20:25:40 +1000

    feat: Add illustration to /confirm page

---
+++ src/pages/confirm/index.tsx
@@ -0,0 +1,61 @@
+import { SubtleButtonLink } from "@heydovetail/website/components/forms/SubtleButtonLink";
+import { Container } from "@heydovetail/website/components/layout/Container";
+import { Flex } from "@heydovetail/website/components/layout/Flex";
+import { GridWrap } from "@heydovetail/website/components/layout/GridWrap";
+import { Item } from "@heydovetail/website/components/layout/Item";
+import { HeroText } from "@heydovetail/website/components/site/HeroText";
+import { COLORS, TYPICAL_PAGE_WIDTH, TYPICAL_VERTICAL_GAP } from "@heydovetail/website/constants";
+import IllustrationEmail from "@heydovetail/website/pages/confirm/IllustrationEmail";
+import { locations } from "@heydovetail/website/routing/locations";
+import { styled } from "@heydovetail/website/util/styled";
+import * as React from "react";
+import { Helmet } from "react-helmet";
+
+export default class extends React.PureComponent {
+  public render() {
+    return (
+      <>
+        <Helmet>
+          <title>Confirm your email address – Dovetail</title>
+        </Helmet>
+        <Container maxWidth={TYPICAL_PAGE_WIDTH} verticalPadding={TYPICAL_VERTICAL_GAP / 2}>
+          <GridWrap gap={32} growItems>
+            <Item style={{ maxWidth: 416 }}>
+              <Flex gap={40} layout="column">
+                <Item>
+                  <HeroText center={false} title="You’ve got mail." />
+                </Item>
+                <Item>
+                  <p>
+                    We need to confirm your email address. Please follow the instructions conveniently located in your inbox.
+                  </p>
+                </Item>
+                <Item>
+                  <ButtonWrapper>
+                    <SubtleButtonLink location={locations.resendConfirmation()}>
+                      Not there? Resend confirmation
+                    </SubtleButtonLink>
+                  </ButtonWrapper>
+                </Item>
+              </Flex>
+            </Item>
+            <Item>
+              <IllustrationEmail size="100%" />
+            </Item>
+          </GridWrap>
+        </Container>
+      </>
+    );
+  }
+}
+
+const ButtonWrapper = styled("span", {
+  color: COLORS.i40,
+  marginLeft: "-8px",
+
+  $nest: {
+    "&:hover": {
+      color: COLORS.purple
+    }
+  }
+});
