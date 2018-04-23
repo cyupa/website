eec8c290c34a48ebb5dafa57e1cbbaf18235bd8e
Benjamin Humphrey <humphreybc@gmail.com>
2018-04-23 15:15:54 +1000

    fix: Storybook

--- src/components/site/Header/__stories__/index.stories.tsx
+++ src/components/site/Header/__stories__/index.stories.tsx
@@ -3,6 +3,5 @@ import * as React from "react";
 import { Header } from "../";
 
 storiesOf("Site/Header", module)
-  .add("light", () => <Header loggedIn={false} />)
-  .add("dark", () => <Header loggedIn={false} dark />)
-  .add("with avatar", () => <Header loggedIn />);
+  .add("light", () => <Header />)
+  .add("dark", () => <Header dark />);
