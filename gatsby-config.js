2127c48938d07763e4f0339d297d62183fec0411
Benjamin Humphrey <humphreybc@gmail.com>
2018-04-26 20:30:03 +1000

    fix: Remove Sentry to see if that’s causing the Fetch as Google blank page

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,16 +43,16 @@ module.exports = {
         trackingId: "UA-97199079-2"
       }
     },
-    {
-      resolve: "gatsby-plugin-sentry",
-      options: {
-        dsn: process.env.SENTRY_DSN || "",
-        config: {
-          environment: process.env.SENTRY_ENVIRONMENT,
-          release: process.env.SENTRY_RELEASE
-        }
-      }
-    },
+    // {
+    //   resolve: "gatsby-plugin-sentry",
+    //   options: {
+    //     dsn: process.env.SENTRY_DSN || "",
+    //     config: {
+    //       environment: process.env.SENTRY_ENVIRONMENT,
+    //       release: process.env.SENTRY_RELEASE
+    //     }
+    //   }
+    // },
     {
       resolve: "gatsby-plugin-nprogress",
       options: {
