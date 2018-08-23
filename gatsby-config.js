4ed884546190be8982f3e2250e2ab499abff2811
Benjamin Humphrey <benjamin@dovetailapp.com>
2018-08-23 13:51:34 +1000

    chore: Remove gastby-react-next

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-react-next",
     "gatsby-plugin-remove-trailing-slashes",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-sharp",
