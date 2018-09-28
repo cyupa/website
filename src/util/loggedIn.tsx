c46256cee083270508f0437a6d6d23ce225ca9dc
Bradley Ayers <brad@dovetailapp.com>
2018-09-28 21:35:02 +1000

    fix: guard against window.localStorage being null

--- src/util/loggedIn.tsx
+++ src/util/loggedIn.tsx
@@ -3,7 +3,7 @@ export const getLoggedInStateFromLocalStorage = () => {
     // tslint:disable-next-line:no-any
     typeof (window as any) !== "undefined" &&
     // tslint:disable-next-line:no-any
-    (window.localStorage as any) !== undefined &&
+    (window.localStorage as any) != null &&
     window.localStorage.getItem("is-logged-in") !== null
   );
 };
